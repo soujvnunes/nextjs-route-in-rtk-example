34613df91a32479a2354ee741a386c6af9b4babe
Victor <soujvnunes@gmail.com>
2025-04-10 10:03:28 -0300

    app/provider: unsubscrive listeners

--- app/provider.tsx
+++ app/provider.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useRef } from 'react'
+import { useEffect, useRef } from 'react'
 import { Provider as ReduxProvider } from 'react-redux'
 
 import { ConfigProvider, theme as antdTheme, type ThemeConfig } from 'antd'
@@ -8,16 +8,25 @@ import { AntdRegistry } from '@ant-design/nextjs-registry'
 import '@ant-design/v5-patch-for-react-19'
 
 import initStore, { type AppStore } from './store'
+import { setupListeners } from '@reduxjs/toolkit/query/react'
 
 export default function Provider(
   props: Readonly<{ children: React.ReactNode }>,
 ) {
-  const storeRef = useRef<AppStore>(null)
+  const store = useRef<AppStore>(null)
 
-  if (!storeRef.current) storeRef.current = initStore()
+  if (!store.current) store.current = initStore()
+
+  useEffect(() => {
+    if (store.current == null) return
+
+    const unsubscribe = setupListeners(store.current.dispatch)
+
+    return unsubscribe
+  }, [])
 
   return (
-    <ReduxProvider store={storeRef.current}>
+    <ReduxProvider store={store.current}>
       <AntdRegistry>
         <ConfigProvider
           theme={theme}
